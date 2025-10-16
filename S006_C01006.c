348ce48d673b9c207084e5fa1bb1db9e170ea561
xNamik4ze <iceminato31@gmail.com>
2025-10-16 08:13:57 +0700

    Phep toan co ban 1

---
+++ S006_C01006.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (b == 0) printf("%d", 0);
+    else printf("%d %d %d %.2f %d", a + b, a - b, a * b, (double)a / b, a % b);
+    return 0;
+}
