04f0dda5404fda7dc1c360de4809f29b0c53c017
xNamik4ze <iceminato31@gmail.com>
2025-10-16 07:46:07 +0700

    Gap doi

---
+++ S001_C01001.c
@@ -0,0 +1,12 @@
+/*
+Cho số tự nhiên N không quá 7 chữ số. Hãy in ra giá trị gấp đôi của N.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", n * 2);
+    return 0;
+}
