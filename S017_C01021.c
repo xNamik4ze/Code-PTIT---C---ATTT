5d74723b92a62fc6c7767cf5c0866ed086083517
xNamik4ze <iceminato31@gmail.com>
2025-10-16 14:11:31 +0700

    Tong chu so - 1

--- S017_C01021.c
+++ S017_C01021.c
@@ -4,12 +4,11 @@ int main() {
     int n;
     scanf("%d", &n);
     int sum = 0;
-    while (n > 9) {
+    while (n > 0) {
         int temp = n % 10;
         sum += temp;
         n /= 10;
     }
-    sum += n;
     printf("%d", sum);
     return 0;
 }
